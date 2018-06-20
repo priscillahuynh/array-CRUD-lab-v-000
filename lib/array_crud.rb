d17da7e3e6157a4398c0a1a6768260dc6b5c4f81
Priscilla H <priscillamhuynh@gmail.com>
2018-06-20 12:11:21 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-array<<"wow"
-array<< "arrays!"
+my_array=Array.new
+my_array<<"red"
+my_array<<"green"
 end
 
 def add_element_to_start_of_array(array, element)
